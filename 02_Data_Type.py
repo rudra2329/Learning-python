2d975d8a180599914f02610fac9c07a39498d3c7
Siva rudra <sivarudra2923@gmail.com>
2025-06-27 22:39:19 +0530

    Update 02_Data_Type.py

--- 02_Data_Type.py
+++ 02_Data_Type.py
@@ -1,3 +1,4 @@
+#code_1
 #To find data type
 X=10 #integer
 Y=12.23 #float
@@ -8,3 +9,10 @@ print(type(Y))
 print(type(Z))
 print(type(A))
 print (type(B))
+
+#code_2
+#To find data type
+X=input("Enter a number:")
+print("The type x is",type (X))
+X=int(X)
+print("The type X is",type(X))

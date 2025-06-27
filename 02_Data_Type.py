8c36203c3aff7b85ec99fa34a05ecc817e85dcc2
Siva rudra <sivarudra2923@gmail.com>
2025-06-27 23:31:39 +0530

    Update 02_Data_Type.py

--- 02_Data_Type.py
+++ 02_Data_Type.py
@@ -22,3 +22,9 @@ print("The type X is",type(X))
 Num1=int(input("Enter the first number:")) 
 Num2=int(input("Enter the second number:")) 
 print("The sum is",Num1+Num2)
+
+#code_4
+#convert number to string
+Roll_no=23
+Roll_no_str=str(Roll_no) 
+print("Rudra Roll no is :"+Roll_no_str)

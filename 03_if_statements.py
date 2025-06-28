254c9bfb5be099c9c36cebbca5f8194c9d98dbb9
Siva rudra <sivarudra2923@gmail.com>
2025-06-28 12:42:51 +0530

    Update 03_if_statements.py

--- 03_if_statements.py
+++ 03_if_statements.py
@@ -34,3 +34,13 @@ elif(a==b):
     print("Both are equal") 
 else:
     print("B is greater")
+
+#Code_05
+#To find positive or negative
+a=int(input("Enter a number:")) 
+if(a>0):
+    print("Positive") 
+elif(a<0):
+    print("Negative") 
+else:
+    print("Zero")

751bda9d39d544a58ea2d0076955c0e62ecd3008
Siva rudra <sivarudra2923@gmail.com>
2025-07-05 22:10:02 +0530

    Create 06_loop_in_list.py

---
+++ 06_loop_in_list.py
@@ -0,0 +1,9 @@
+#code_01
+fruits = ["apple", "banana", "mango"]#same for code_02
+
+for fruit in fruits:
+    print(fruit)movies = ["Leo", "Jailer", "Vikram"]
+
+#code_02
+for i in range(len(movies)):
+    print("Movie", i+1, ":", movies[i])
